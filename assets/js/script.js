a6064af57353925abe9d1bb1ac6d3e2f2a30f017
Pandu-5 <118616769+Pandu-5@users.noreply.github.com>
2023-01-02 20:21:13 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,33 @@ function myFunction() {
     var x = document.getElementById("header-content");
     if (x.style.display === "none") {
         x.style.display = "flex";
-        x.style.transition = "all 3.5s ease-in-out";
     } else {
         x.style.display = "none";
     }
 }
+
+function myprofFun() {
+    var x = document.getElementById("content");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showinput() {
+    var x = document.getElementById("check");
+    var y = document.getElementById("checkbtn");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "flex";
+    }
+}
+
+
 
 const navBar = document.querySelector(".nav"),
     menuBtns = document.querySelectorAll(".menu-icon"),
