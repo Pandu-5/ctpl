1643735d9d82ea04b35de1bd5dbab89d605e9a42
Pandu-5 <118616769+Pandu-5@users.noreply.github.com>
2023-01-03 10:38:59 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+
+function myFunction() {
+    var x = document.getElementById("header-content");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myprofFun() {
+    var x = document.getElementById("content");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showinput() {
+    var x = document.getElementById("check");
+    var y = document.getElementById("checkbtn");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "flex";
+    }
+}
+
+
+function popup() {
+    var x = document.getElementById("login-form");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
+const navBar = document.querySelector(".nav"),
+    menuBtns = document.querySelectorAll(".menu-icon"),
+    overlay = document.querySelector(".overlay");
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
+});
